82ff910e2c725ecbf08b6037ef39b51914997883
NadiaY123456 <62516100+NadiaY123456@users.noreply.github.com>
2025-02-15 11:51:14 -0800

    animation works

---
+++ MatheMagicApp/Views/Scene Functions/setupCharacterWithComponents.swift
@@ -0,0 +1,37 @@
+import CoreLib
+import RealityKit
+import AnimLib
+//
+//  setupCharacterWithComponents.swift
+//  MatheMagic
+//
+//
+
+@MainActor func setupCharacterWithComponents(entityDictionaryID: String) -> Entity {
+    var entity = Entity()
+    if let template = entityModelDictionaryCore[entityDictionaryID] {
+        entity = template.entity
+
+        // Event Component
+        entity.components[EventComponent.self] = EventComponent()
+
+        // Data Center Component
+        if let dataManager = template.dataManager {
+            entity.components[DataCenterComponent.self] = DataCenterComponent(dataManager: dataManager)
+        }
+
+        // Brain Component
+        entity.components[BrainComponent.self] = BrainComponent()
+
+        // Travel Component
+        entity.components[TravelComponent.self] = TravelComponent()
+
+        // Custom Animation Component
+        entity.components[CustomAnimationComponent.self] = CustomAnimationComponent()
+
+        // Animation Component
+        entity.components.set(AnimationComponent())
+
+    } else { AppLogger.shared.error("Error: did not find \(entityDictionaryID) key in entityTemplateDictionary") }
+    return entity
+}
